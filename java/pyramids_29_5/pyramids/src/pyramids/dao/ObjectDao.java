dfb797db578c91c23c1e76003e3524ce9be693b3
azizmousa <azizmousa1010@gmail.com>
2021-05-29 23:18:01 +0200

    conver load method to return a generic type

--- java/pyramids_29_5/pyramids/src/pyramids/dao/ObjectDao.java
+++ java/pyramids_29_5/pyramids/src/pyramids/dao/ObjectDao.java
@@ -1,10 +1,7 @@
 package pyramids.dao;
-
-import pyramids.model.Pyramid;
-
 import java.util.List;
 
 public interface ObjectDao {
 
-    List<Pyramid> loadPyramidFromCSV(String path, boolean hasHeader);
+    List<?> loadObjectsFromCSV(String path, boolean hasHeader);
 }
