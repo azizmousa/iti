c5c07bfa386090864666d91c64d781d39c2b6359
azizmousa <azizmousa1010@gmail.com>
2021-05-29 22:54:06 +0200

    create the main class

---
+++ java/pyramids_29_5/pyramids/src/pyramids/main/Runner.java
@@ -0,0 +1,22 @@
+package pyramids.main;
+
+import pyramids.dao.PyramidDao;
+import pyramids.model.Pyramid;
+
+import java.util.List;
+
+public class Runner {
+
+    public static void main(String [] args){
+        String filePath = "pyramids.csv";
+        PyramidDao pyramidDao = new PyramidDao();
+        List<Pyramid> pyramids = pyramidDao.loadPyramidFromCSV(filePath, true);
+        for(Pyramid p : pyramids){
+            System.out.println(p);
+        }
+        System.out.println("Number of pyramids = " + pyramids.size());
+    }
+
+
+
+}
