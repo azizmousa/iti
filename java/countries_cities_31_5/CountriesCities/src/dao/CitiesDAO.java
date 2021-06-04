6487eaa575a5fd2de26393b04cf5a3b3833c0bc9
azizmousa <azizmousa1010@gmail.com>
2021-06-04 22:49:22 +0200

    implement cities DAO class

---
+++ java/countries_cities_31_5/CountriesCities/src/dao/CitiesDAO.java
@@ -0,0 +1,38 @@
+package dao;
+
+import model.City;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CitiesDAO implements ObjectDAO{
+
+    @Override
+    public List<City> readCSVFile(String path, boolean hasHeader) {
+        List<City> cities = new ArrayList<>();
+        File file = new File(path);
+        Scanner scanner = null;
+        try{
+            scanner = new Scanner(file);
+
+            if(hasHeader)
+                scanner.nextLine();
+
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                String [] features = line.split(",");
+                City city = new City(Integer.parseInt(features[0]), Integer.parseInt(features[1]), features[2],
+                        Boolean.parseBoolean(features[3]), Long.parseLong(features[4]));
+                cities.add(city);
+            }
+        }catch (FileNotFoundException fileNotFoundException){
+            System.out.println(path + " File Not Found!!!.");
+        }finally {
+            if(scanner != null)
+                scanner.close();
+        }
+        return cities;
+    }
+}
