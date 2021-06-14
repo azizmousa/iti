b9a08752ba031472d0992aa5ba8dc03fafcfa77e
azizmousa <azizmousa1010@gmail.com>
2021-06-14 20:24:44 +0200

    print data summary with tablesaw and joinery

--- java/api_6_6/titanic/src/main/java/com/aziz/titanic/runner/Runner.java
+++ java/api_6_6/titanic/src/main/java/com/aziz/titanic/runner/Runner.java
@@ -1,14 +1,31 @@
 package com.aziz.titanic.runner;
 
+import joinery.DataFrame;
 import tech.tablesaw.api.Table;
 
 import java.io.IOException;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Runner {
     public static void main(String [] args){
         try{
+            // Explore data with tablewas library
             Table titanic = Table.read().csv(Runner.class.getClassLoader().getResource("dataset/titanic.csv"));
             System.out.println(titanic.structure().printAll());
+            System.out.println(titanic.summary().toString());
+            System.out.println(titanic.column("survived").summary().toString());
+
+            // Explore data with joinery library
+            System.out.println("joinery Dataframe Object:");
+            DataFrame<Object> df = DataFrame.readCsv(Objects.requireNonNull(Runner.class
+                    .getClassLoader().getResourceAsStream("dataset/titanic.csv")));
+            System.out.println(df.toString());
+            System.out.println("Joinery DataFrame Summary");
+            System.out.println(df.describe().toString());
+            System.out.println("Joinery DataFrame Columns");
+            System.out.println(df.columns().toString());
+            System.out.println("Number of pClasses = " + df.col("pclass").stream().distinct().count());
         }catch (IOException ioe){
             ioe.printStackTrace();
         }
