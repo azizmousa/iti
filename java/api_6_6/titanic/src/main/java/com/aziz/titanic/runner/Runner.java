22b9ba931b89d4445e09bf0f5a4599a3d506f209
azizmousa <azizmousa1010@gmail.com>
2021-06-14 19:03:14 +0200

    load titanic csv file

--- java/api_6_6/titanic/src/main/java/com/aziz/titanic/runner/Runner.java
+++ java/api_6_6/titanic/src/main/java/com/aziz/titanic/runner/Runner.java
@@ -1,7 +1,16 @@
 package com.aziz.titanic.runner;
 
+import tech.tablesaw.api.Table;
+
+import java.io.IOException;
+
 public class Runner {
     public static void main(String [] args){
-
+        try{
+            Table titanic = Table.read().csv(Runner.class.getClassLoader().getResource("dataset/titanic.csv"));
+            System.out.println(titanic.structure().printAll());
+        }catch (IOException ioe){
+            ioe.printStackTrace();
+        }
     }
 }
